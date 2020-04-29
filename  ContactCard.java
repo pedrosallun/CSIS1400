98455608fb18b1182e5c88ee5f66fb21639e617c
pedrosallun <64516204+pedrosallun@users.noreply.github.com>
2020-04-28 22:58:01 -0600

    Add files via upload

---
+++  ContactCard.java
@@ -0,0 +1,30 @@
+//Pedro Sallun
+
+public class ContactCard
+{
+	public static void main(String[] args) 
+	{
+      String FirstName;
+      String LastName;
+      String Adress1;
+      String Adress2;
+      String City;
+      String State;
+      String ZipCode;
+      String PhoneNumber;
+      String Email;
+      String Age;
+      String Height;
+      String Weight;
+      String JobTitle;
+      String Income;
+      
+      System.out.println("---------------------------------------");
+      System.out.println("Please enter the following information:");
+      System.out.println("---------------------------------------");
+
+      
+
+      
+	}
+}
